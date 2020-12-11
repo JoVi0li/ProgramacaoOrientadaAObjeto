a8bb59538f6150b2c50a1a25e82ff34e522a1e67
DEV <Jovi.oli04@gmail.com>
2020-12-11 08:43:33 -0300

    Finalizado

---
+++ AulaPOO_PolimosrfismoSobrecarga/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AulaPOO_PolimosrfismoSobrecarga.classes;
+
+namespace AulaPOO_PolimosrfismoSobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            funcionario.Mostrar();
+            Console.Beep();
+            funcionario.Mostrar("João");
+            Console.Beep();
+            funcionario.Mostrar(8);
+            Console.Beep();
+        }
+    }
+}
