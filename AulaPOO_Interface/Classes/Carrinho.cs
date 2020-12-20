95bf60dbf0ce04aef8661052231d631aeacc8c31
João Vitor <Jovi.oli04@gmail.com>
2020-12-20 14:55:15 -0300

    Finalizado

---
+++ AulaPOO_Interface/Classes/Carrinho.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace AulaPOO_Interface.Classes
+{
+    public class Carrinho : ICarrinho
+    {
+
+        List<Produto> carrinho = new List<Produto>();
+
+        public void Alterar(int _codigo, Produto produto)
+        {
+            carrinho.Find(x => x.Codigo == _codigo).Nome = produto.Nome;
+            carrinho.Find(x => x.Codigo == _codigo).Preco = produto.Preco;
+        }
+
+        public void Cadastrar(Produto produto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Deletar(Produto produto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Listar()
+        {
+            foreach( Produto itemCarrinho in carrinho )
+            {
+                Console.WriteLine($"Produto: {itemCarrinho.Nome} || \n Preço: R$ {itemCarrinho.Preco} \n || Código: {itemCarrinho.Codigo}");
+            }
+        }
+    }
+}
---
+++ AulaPOO_Interface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using AulaPOO_Interface.Classes;
+
+namespace AulaPOO_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carrinho carrinho =  new Carrinho();
+
+            Produto p1 = new Produto();
+            p1.Codigo = 1;
+            p1.Nome = "Xbox One";
+            p1.Preco = 2500;
+
+            Produto p2 = new Produto();
+            p2.Codigo = 2;
+            p2.Nome = "Xbox 360";
+            p2.Preco = 500;
+
+            Produto p3 = new Produto();
+            p3.Codigo = 3;
+            p3.Nome = "PlayStation";
+            p3.Preco = 3500;
+
+            carrinho.Cadastrar(p1);
+            carrinho.Listar();
+            carrinho.Cadastrar(p2);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            carrinho.Alterar(p1.Codigo, p3);
+            Console.Beep();
+            carrinho.Listar();
+
+            Console.ResetColor();
+
+        }
+    }
+}
