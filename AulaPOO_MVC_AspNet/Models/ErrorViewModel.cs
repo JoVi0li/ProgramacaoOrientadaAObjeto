bc88ed3aa7c0933a82c139e1fceb8e6c94826523
João Vitor <Jovi.oli04@gmail.com>
2021-01-09 16:32:23 -0300

    Finalizado

---
+++ AulaPOO_MVC_AspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AulaPOO_MVC_AspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
